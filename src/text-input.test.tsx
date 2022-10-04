642c3ef3cd2296756f417c6ea8498d8e4c682b56
0x55 <0xfiftyfive@gmail.com>
2022-10-04 15:19:04 +0700

    Add format test

--- src/text-input.test.tsx
+++ src/text-input.test.tsx
@@ -1,8 +1,9 @@
 import 'react-native';
-import React from 'react';
+import React, { ReactComponentElement, ReactElement } from 'react';
 
 import { fireEvent, render, screen } from '@testing-library/react-native';
 import { TextInput } from './text-input';
+import { ReactTestInstance } from 'react-test-renderer';
 
 describe('TextInput', () => {
   test('onChangeText', () => {
@@ -17,25 +18,51 @@ describe('TextInput', () => {
     expect(handleChangeText).toBeCalledWith('some text');
   });
 
-  test('onChange', () => {
+  test('pattern', () => {
     const handleChange = jest.fn();
 
     render(
       <TextInput pattern="\d\d\d" placeholder="test" onChange={handleChange} />,
     );
 
-    const textInput = screen.getByPlaceholderText('test');
+    const el = screen.getByPlaceholderText('test');
 
-    fireEvent(textInput, 'onChange', {
-      nativeEvent: { text: '12' },
-    });
-
+    fireOnChange(el, '');
     expect(handleChange).toHaveBeenCalledWith(null);
 
-    fireEvent(textInput, 'onChange', {
-      nativeEvent: { text: '123' },
-    });
+    fireOnChange(el, '12');
+    expect(handleChange).toHaveBeenCalledWith(null);
 
+    fireOnChange(el, '123');
     expect(handleChange).toHaveBeenCalledWith('123');
   });
+
+  test('format', () => {
+    const handleChange = jest.fn();
+
+    render(
+      <TextInput
+        format="0000-00-00"
+        placeholder="test"
+        onChange={handleChange}
+        value="1970"
+      />,
+    );
+
+    const el = screen.getByPlaceholderText('test');
+    expect(el.props.value).toBe('1970-');
+
+    fireOnChange(el, '');
+    expect(handleChange).toHaveBeenCalledWith(null);
+
+    fireOnChange(el, '1970-0');
+    expect(handleChange).toHaveBeenCalledWith(null);
+
+    fireOnChange(el, '1970-01-01');
+    expect(handleChange).toHaveBeenCalledWith('1970-01-01');
+  });
 });
+
+function fireOnChange(el: ReactTestInstance, text: string) {
+  fireEvent(el, 'onChange', { nativeEvent: { text } });
+}
